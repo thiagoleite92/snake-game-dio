05ed39dbde4f20a828ec3e021675949c945b5702
Thiago Leite <leite.tjs@gmail.com>
2021-08-20 08:21:59 -0300

    moivmento da serpente

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let canvas = document.getElementById('snake');
-let context = canvas.getContext('2d');
+let canvas = document.getElementById("snake");
+let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
 
@@ -8,17 +8,40 @@ snake[0] = {
   y: 8 * box
 }
 
-function createBG() {
-  context.fillStyle = 'lightgreen';
-  context.fillRect(0, 0, 16 * box, 16 * box);
-}
+let direction = "right"
 
-function createSnake() {
-  for ( let i = 0; i < snake.length; i += 1) {
-    context.fillStyle = 'green';
+function criarBG() {
+  context.fillStyle = "lightgreen";
+  context.fillRect(0, 0, 16 * box, 16 * box)
+};
+
+function criarCobrinha() {
+  for (let  i = 0; i < snake.length; i++) {
+    context.fillStyle = "green";
     context.fillRect(snake[i].x, snake[i].y, box, box);
   }
 }
 
-createBG();
-createSnake();
+function iniciarJogo() {
+  criarBG();
+  criarCobrinha();
+
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+  if (direction == "right") {
+    snakeX += box;
+  }
+
+  snake.pop()
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY
+  }
+
+  snake.unshift(newHead)
+}
+
+let jogo = setInterval(iniciarJogo, 100)
+
